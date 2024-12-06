da02df3c386fa461171f8197d8a53ea2866bc89a
suriyak2001 <suriyak2030@gmail.com>
2024-12-06 10:18:30 +0530

    used module to export and import cart and products

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,18 @@
-const cart = [];
+export const cart = [];
+
+export function addToCart(productId) {
+    let matchingItem = '';
+    cart.forEach((item) => {
+        if (productId === item.productId) {
+        matchingItem = item;
+        }
+    });
+    if (matchingItem) {
+        matchingItem.quantity += 1;
+    } else {
+        cart.push({
+        productId,
+        quantity: 1,
+        });
+    }
+}
--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,6 @@
-// accumulator pattern
+import {cart, addToCart} from '../data/cart.js';
+import {products} from '../data/products.js';
+
 let productHTML = '';
 
 products.forEach( (product) => {
@@ -56,32 +58,22 @@ products.forEach( (product) => {
 document.querySelector('.js-product-grid')
 .innerHTML = productHTML;
 
+function updateCartQuantity() {
+  let cartQuantity = 0;
+  cart.forEach((item) => {
+   cartQuantity += item.quantity;
+  });
+  document.querySelector('.js-display-cart-quantity')
+   .innerHTML = cartQuantity;
+}
+
 document.querySelectorAll('.js-add-to-cart')
  .forEach((button) => {
    button.addEventListener('click', () => {
     const productId = button.dataset.productId;
 
-    let matchingItem = '';
-    cart.forEach((item) => {
-      if (productId === item.productId) {
-       matchingItem = item;
-      }
-    });
-    if (matchingItem) {
-      matchingItem.quantity += 1;
-    } else {
-      cart.push({
-        productId: productId,
-        quantity: 1,
-      });
-    }
-
-    let cartQuantity = 0;
-    cart.forEach((item) => {
-     cartQuantity += item.quantity;
-    });
-    document.querySelector('.js-display-cart-quantity')
-     .innerHTML = cartQuantity;
-   });
+    addToCart(productId);
+    updateCartQuantity(); 
   });
+});
 
