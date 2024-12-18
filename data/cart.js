97766b6076cdab9d0377b86159f01f799691c9d7
suriyak2001 <suriyak2030@gmail.com>
2024-12-18 07:17:47 +0530

    save and generate html for delivery options

--- data/cart.js
+++ data/cart.js
@@ -3,6 +3,7 @@ export let cart = JSON.parse(localStorage.getItem('cart'));
 if(!cart) {
     cart = [{productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
      quantity: 0,
+     deliveryOptionId: '3',
     }];
 }
 
@@ -23,7 +24,8 @@ export function addToCart(productId) {
     } else {
         cart.push({
         productId,
-        quantity: 1,
+        quantity: 2,
+        deliveryOptionId: '1',
         });
     }
     saveToStorage();
