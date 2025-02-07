36d5b9f956c21f776f30607e814caf794a8eb503
suriyak2001 <suriyak2030@gmail.com>
2025-02-07 21:54:56 +0530

    added classes to the product object using map

--- data/products.js
+++ data/products.js
@@ -9,6 +9,22 @@ export function getProduct(productId) {
   return matchingProduct;
 }
 
+class Product {
+  id;
+  image;
+  name;
+  rating;
+  priceCents;
+
+  constructor(productDetails) {
+    this.id = productDetails.id;
+    this.image = productDetails.image;
+    this.name = productDetails.name;
+    this.rating = productDetails.rating;
+    this.priceCents = productDetails.priceCents;
+  }
+}
+
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -668,4 +684,7 @@ export const products = [
       "mens"
     ]
   }
-];
+].map( (productDetails) => {
+  return new Product(productDetails);
+});
+console.log(products);
