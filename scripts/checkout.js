51cdb24d8a999fb3e484e0c142ecb2307fda6e19
suriyak2001 <suriyak2030@gmail.com>
2025-02-25 09:05:11 +0530

    practicing backend intrpduction

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,6 @@
 import { renderOrderSummary } from './checkout/ordersummary.js';
 import { renderPaymentSummary } from './checkout/paymentsummary.js';
+import '../data/backend-practice.js';
 //import '../data/cart-class.js';
 
 renderOrderSummary();
