27a65a749a663fe9c72366c38f40be5ac3ffb479
suriyak2001 <suriyak2030@gmail.com>
2025-02-25 09:49:17 +0530

    loaded products from the backend

--- data/products.js
+++ data/products.js
@@ -46,7 +46,7 @@ class Clothing extends Product {
     super(productDetails);
     this.sizeChartLink = productDetails.sizeChartLink;
   }
-  
+
   extraInfoHTML() {
     return `
     <a href="${this.sizeChartLink}" target= "_blank">
@@ -55,7 +55,28 @@ class Clothing extends Product {
   }
 }
 
-export const products = [
+export let products = [];
+
+export function loadProducts(fun) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener('load', () => {
+    products = JSON.parse(xhr.response).map( (productDetails) => {
+      if(productDetails.type === 'clothing') {
+        return new Clothing(productDetails);
+      }
+      return new Product(productDetails);
+    });
+    
+    fun();
+  });
+
+  xhr.open('GET', 'https://supersimplebackend.dev/products');
+  xhr.send();
+}
+loadProducts();
+
+/*export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     image: "images/products/athletic-cotton-socks-6-pairs.jpg",
@@ -719,4 +740,4 @@ export const products = [
     return new Clothing(productDetails);
   }
   return new Product(productDetails);
-});
+});*/
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,7 +1,11 @@
 import { renderOrderSummary } from './checkout/ordersummary.js';
 import { renderPaymentSummary } from './checkout/paymentsummary.js';
-import '../data/backend-practice.js';
+import { loadProducts } from '../data/products.js';
+
+//import '../data/backend-practice.js';
 //import '../data/cart-class.js';
 
-renderOrderSummary();
-renderPaymentSummary();
+loadProducts( () => {
+    renderOrderSummary();
+    renderPaymentSummary();
+});
